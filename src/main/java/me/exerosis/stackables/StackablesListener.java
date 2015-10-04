14bf60a540b305908d777ac7e8da8fc0cedab6de
Exerosis <theexerosisgaming@gmail.com>
2015-10-04 10:51:01 -0400

    ~Updates

--- src/main/java/me/exerosis/stackables/StackablesListener.java
+++ src/main/java/me/exerosis/stackables/StackablesListener.java
@@ -21,66 +21,6 @@ import org.bukkit.inventory.Inventory;
  */
 public class StackablesListener implements Listener {
     /**
-     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
-     *
-     * @param event {@linkplain InventoryClickEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onClick(InventoryClickEvent event) {
-        updateInventoryLater(event.getWhoClicked());
-    }
-
-    /**
-     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
-     *
-     * @param event {@linkplain InventoryDragEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onDrag(InventoryDragEvent event) {
-        updateInventoryLater(event.getWhoClicked());
-    }
-
-    /**
-     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
-     *
-     * @param event {@linkplain PlayerPickupItemEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onPickup(PlayerPickupItemEvent event) {
-        updateInventoryLater(event.getPlayer());
-    }
-
-    /**
-     * Sets the {@linkplain Player}'s {@linkplain Inventory}'s stack size to the max Minecraft allows.
-     *
-     * @param event {@linkplain PlayerJoinEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        setInventoryStackSize(event.getPlayer().getInventory());
-    }
-
-    /**
-     * Sets the opened {@linkplain Inventory}'s stack size to the max Minecraft allows.
-     *
-     * @param event {@linkplain InventoryOpenEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onOpenInventory(InventoryOpenEvent event) {
-        setInventoryStackSize(event.getInventory());
-    }
-
-    /**
-     * Sets the hopper's {@linkplain Inventory} to the max Minecraft allows.
-     *
-     * @param event {@linkplain InventoryMoveItemEvent} - A param for {@linkplain Bukkit}'s event system.
-     */
-    @EventHandler
-    public void onHopper(InventoryMoveItemEvent event) {
-        setInventoryStackSize(event.getDestination());
-    }
-
-    /**
      * Sets the max stack size of the {@linkplain Inventory} to the max Minecraft allows.
      *
      * @param inventory {@linkplain Inventory} - Sets the {@linkplain Inventory}'s stack size to the max Minecraft allows.
@@ -88,7 +28,6 @@ public class StackablesListener implements Listener {
     public static void setInventoryStackSize(Inventory inventory) {
         inventory.setMaxStackSize(Stackables.MAX_STACK);
     }
-
 
     /**
      * If the entity is a player it will update the players inventory after waiting one tick.
@@ -100,5 +39,65 @@ public class StackablesListener implements Listener {
             return;
         Player player = (Player) entity;
         Bukkit.getScheduler().runTaskLater(Stackables.getPlugin(), player::updateInventory, 1);
+    }
+
+    /**
+     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
+     *
+     * @param event {@linkplain InventoryClickEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onClick(InventoryClickEvent event) {
+        updateInventoryLater(event.getWhoClicked());
+    }
+
+    /**
+     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
+     *
+     * @param event {@linkplain InventoryDragEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onDrag(InventoryDragEvent event) {
+        updateInventoryLater(event.getWhoClicked());
+    }
+
+    /**
+     * Updates the {@linkplain Player}'s {@linkplain Inventory} after one tick.
+     *
+     * @param event {@linkplain PlayerPickupItemEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onPickup(PlayerPickupItemEvent event) {
+        updateInventoryLater(event.getPlayer());
+    }
+
+    /**
+     * Sets the {@linkplain Player}'s {@linkplain Inventory}'s stack size to the max Minecraft allows.
+     *
+     * @param event {@linkplain PlayerJoinEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        setInventoryStackSize(event.getPlayer().getInventory());
+    }
+
+    /**
+     * Sets the opened {@linkplain Inventory}'s stack size to the max Minecraft allows.
+     *
+     * @param event {@linkplain InventoryOpenEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onOpenInventory(InventoryOpenEvent event) {
+        setInventoryStackSize(event.getInventory());
+    }
+
+    /**
+     * Sets the hopper's {@linkplain Inventory} to the max Minecraft allows.
+     *
+     * @param event {@linkplain InventoryMoveItemEvent} - A param for {@linkplain Bukkit}'s events system.
+     */
+    @EventHandler
+    public void onHopper(InventoryMoveItemEvent event) {
+        setInventoryStackSize(event.getDestination());
     }
 }
